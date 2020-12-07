a0f4c8757be0a1f2a5c1f2deb5a0a384e303e927
pilarguz10 <pilar1494@hotmail.com>
2020-12-07 21:02:35 +0000

    first commit

--- exercises/05-Defining-vs-Calling-a-function/app.js
+++ exercises/05-Defining-vs-Calling-a-function/app.js
@@ -1,5 +1,10 @@
 //Define the function called "multi" that expects 2 parameters:
+function multi (numero1, numero2)
+{
+    var result = numero1 * numero2;
 
+    return result;
+}
 
   //don't edit anything below this line
   var returnValue = multi(7,53812212);
--- exercises/07-Arrow-Functions/app.js
+++ exercises/07-Arrow-Functions/app.js
@@ -1,4 +1,18 @@
 
+   const rapid = (str) => {for (let index = 0; index < str.length; index++)
+    {
+       if(index != 'a' || index != "e" || index != "i" || index != "o" || index != "u" ) 
+       {
+        const element = str[index];
+       }
+      
+      return element.toUpperCase(); 
+   }
+   
+   
+   
    // From this line up Do not change code below
    let str = "John";
    console.log(rapid(str));
+
+
